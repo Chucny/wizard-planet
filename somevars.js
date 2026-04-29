3bdcd4e8725f2abc2cac12a7bb9c44e3310d4f19
Chucny <166989589+Chucny@users.noreply.github.com>
2026-04-29 10:31:10 +0300

    Add initial variables for game configuration

---
+++ somevars.js
@@ -0,0 +1,12 @@
+let CURRENT_RAID_SHAPE_SIDES = 3; 
+let BOSS_HP_VAR = 2000;
+
+
+// antcheat stuff
+
+const ALLOW_COMPUTER = true; // Set to false to block PC/Laptop logins
+const BANNED_PLAYERS = ["william"]; // Add usernames to ban here
+
+
+
+
