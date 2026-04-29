36272ddb841b6eaf242e550430c28edf588e256e
Chucny <166989589+Chucny@users.noreply.github.com>
2026-04-29 12:24:06 +0300

    Fix anticheat settings and update computer login flag

--- somevars.js
+++ somevars.js
@@ -2,9 +2,9 @@ let CURRENT_RAID_SHAPE_SIDES = 3;
 let BOSS_HP_VAR = 2000;
 
 
-// antcheat stuff
+// anticheat stuff
 
-const ALLOW_COMPUTER = true; // Set to false to block PC/Laptop logins
+const ALLOW_COMPUTER = false; // Set to false to block PC/Laptop logins
 const BANNED_PLAYERS = ["william"]; // Add usernames to ban here
 
 
