ea646ca2ccae7c7069d48e54e63b8e82b6c5018f
Chucny <166989589+Chucny@users.noreply.github.com>
2026-05-21 12:03:44 +0300

    Change ALLOW_COMPUTER to false

--- somevars.js
+++ somevars.js
@@ -4,7 +4,7 @@ let BOSS_HP_VAR = 2100;
 
 // anticheat stuff
 
-const ALLOW_COMPUTER = true; // Set to false to block PC/Laptop logins
+const ALLOW_COMPUTER = false; // Set to false to block PC/Laptop logins
 const BANNED_PLAYERS = []; // Add usernames to ban here
 
 
